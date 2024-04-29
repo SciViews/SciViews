dcea695670c50b0505e4a936b9a102b96a8e539b
Philippe Grosjean <phgrosjean@sciviews.org>
2024-04-29 17:34:34 +0200

    Further chatbot configuration

--- R/ask.R
+++ R/ask.R
@@ -71,6 +71,7 @@ ask <- function(question, context = NULL,
       httr2::req_body_json(list(
         model = model,
         messages = list(
+          list(role = "system", content = "<s>[INST]I am SciViews chatbot, a large language model. I am trained to answer questions and provide information. My answers are about about the R language, statistics and data science. I answer with precision. I stop answering if I am not sure to tell the truth. The transcript uses Markdown tags. R code is between ```R and ``` tags. Outputs are between ``` and ```. I explain code. I give short examples. My code is executable. I use ggplot() preferrably to plot().[/INST]"),
           list(role = "user", content = question)
         ),
         `tool-choice` = "none",
@@ -112,8 +113,8 @@ ask <- function(question, context = NULL,
     markdown_on(old_mkdown)
 
     # Extract examples
-    examples <- gsub("Output:\n\n```[Rr]\n", "Output:\n\n```\n", answer)
-    examples <- strsplit(answer, "```", fixed = TRUE)[[1]]
+    examples <- gsub("Output ?:\n+```[Rr]\n", "Output:\n\n```\n", answer)
+    examples <- strsplit(examples, "```", fixed = TRUE)[[1]]
     examples <- examples[grepl("^[Rr]\n", examples)]
     examples <- sub("^[Rr]\n", "", examples)
     examples <- paste(examples, collapse = "\n")
@@ -131,6 +132,48 @@ ask <- function(question, context = NULL,
   invisible(chat_data$answer)
 }
 
+#ask(r"--(<s>[INST]I am SciViews chatbot, a large language model. I am trained to answer questions and provide information. My answers are about about the R language, statistics and data science. I answer with precision. I stop answering if I am not sure to tell the truth. The transcript uses Markdown tags. R code is between ```R and ``` tags. Outputs are between ``` and ```. I explain code. I give short examples. My code is executable. I use ggplot() preferrably to plot().[/INST][INST]How to determine which model is better for nested models using an ANOVA?[\INST])--", verbose = TRUE)
+
+#ask("How to decide which model is better for nested models using an ANOVA? Do not use the aov1 package.")
+
+#ask("Explain subsetting in R in simple terms.")
+
+#ask("Can I use R^2 to compare models of different conplexities?")
+
+#ask("Which metric is better for species x stations datasets?")
+
+# Explique ou est l'erreur dans ce code: y <- c(1, 5, 7, NA, -Inf, 8, ).
+# Que fait AIC()? Donne un exemple.
+# Qu'est ce que l'hétéroscédasticité
+# How to determine which model is better for nested models using an ANOVA?
+
+#<s>[INST] Transcription textuelle d'un dialogue, dans laquelle [[USER_NAME]] interagit avec un assistant IA nommé [[AI_NAME]].
+#[[AI_NAME]] est serviable et répond aux demandes de [[USER_NAME]] en français et avec précision.
+#[[AI_NAME]] arrête de répondre si il n'est pas certain de dire la vérité.
+#La transcription comprend uniquement du texte, elle n'inclut pas de balisage comme HTML et Markdown. [/INST]
+
+#[INST] [[USER_NAME]] : Bonjour, [[AI_NAME]] ! [/INST]
+#[[AI_NAME]] : Bonjour [[USER_NAME]] !
+#[INST] [[USER_NAME]] : En quelle année sommes-nous ? [/INST]
+#[[AI_NAME]] : Nous sommes en [[DATE_YEAR]].
+#[INST] [[USER_NAME]] : Veuillez me dire quelle est la plus grande ville d'Europe. [/INST]
+#[[AI_NAME]] : la plus grande ville d'Europe est Moscou, la capitale de la Russie.
+#[INST] [[USER_NAME]] : Qu'est-ce qu'un chat ? [/INST]
+#[[AI_NAME]] : un chat est une espèce domestique de petit mammifère carnivore. C'est la seule espèce domestiquée de la famille des félidés.
+#[INST] [[USER_NAME]] : nommez une couleur. [/INST]
+#[[AI_NAME]] : bleu.
+#[INST] [[USER_NAME]] : Quelle heure est-il ? [/INST]
+#[[AI_NAME]] : Il est [[DATE_TIME]].
+
+
+
+
+
+
+
+
+
+# \ifelse{latex}{\out{$\alpha$}}{\ifelse{html}{\out{<button onclick='window.location.replace("../html/chatbot2.html")'><img src="../../../doc/html/left.jpg"/></button><button onclick='window.location.replace("../html/chatbot3.html")'><img src="../../../doc/html/up.jpg"/></button></br>}}{SciViews chatbot}}
 #' SciViews chatbot
 #'
 #' @description
@@ -145,6 +188,20 @@ ask <- function(question, context = NULL,
 #' @examples
 #' \Sexpr[eval=TRUE,stage=render,results=text]{ if (exists('SciViews.chatbot.page1')) SciViews.chatbot.page1$examples else "# No example" }
 NULL
+
+# @section Test:
+# This is a test...
+# \Sexpr[eval=TRUE,stage=render,results=rd]{ "Test \\\\emph{italic}" }
+# \ifelse{latex}{\out{$\alpha$}}{\ifelse{html}{\out{<a href="../html/chatbot1.html" onclick="window.alert(\"test\"); window.parent.helpNavigate(this ref); return false">link</a>}}{alpha}}
+
+# Test of runnable links: \Sexpr[eval=TRUE,stage=render,results=rd]{ cli::ansi_has_hyperlink_support() }
+#
+# TODO: render differently in text of pdf
+# \Sexpr[eval=TRUE,stage=render,results=verbatim]{ '<a href="../chatbot1" onclick="window.parent.helpNavigate(this.href); return false">Test link injection</a>' }
+
+# cli::cli_text("{.run [Click here](rstudio:run:rlang::last_trace())}")
+# #' \href{"../chatbot1" onclick="window.parent.helpNavigate(this ref); return false"}{Click here to see the answer in the help page}
+# Show md of html file in fenêtre (lite) with curl http://127.0.0.1:23489/open\?url\=<path>
 
 #' SciViews chatbot
 #'
